5f118890149a24a3fb771e3a0417ce84bee00307
Prasad <psdudeman39@gmail.com>
2019-06-21 07:47:47 +0530

    Done with expression and term methods/functions.

--- Recursive_Descent_Parser/Recursive_Descent_Parser.cpp
+++ Recursive_Descent_Parser/Recursive_Descent_Parser.cpp
@@ -11,14 +11,13 @@
 #include <bitset>
 #include <list>
 #include <algorithm>
-using namespace std;
 #define ln "\n"
+using namespace std;
+const char spaceChar = ' ';
+const char endOfStatement = ';';
 
-void issueError(string err) {
-	cout << "======================================================" << endl;
-	cerr << err << endl;
-	cout << "======================================================" << endl;
-}
+//	Set of commands the parser must identify.
+string command[] = { "status", "print", "end" };
 
 /*
 	This is an interface.
@@ -37,8 +36,12 @@ public:
 };
 
 
+/**
+	IdentifierLookupTable implements CompilerTable.
+*/
 class IdentifierLookupTable : public CompilerTable {
 
+private:
 	unordered_map<string, double> table;
 
 public:
@@ -61,6 +64,7 @@ public:
 			if (it.second == value)
 				iList.push_back(make_pair(it.first, it.second));
 		}
+		return iList;
 	}
 
 	double getIdentifierValue(string identifier) {
@@ -68,27 +72,156 @@ public:
 			if (it.first == identifier) return it.second;
 		}
 
-		issueError("Internal parser problem");
+		//	issueError("Internal parser problem");	
+	}
+
+	void insertIdentifierNode(string identifier, double value) {
+		table[identifier] = value;
 	}
 };
 
 
 class IdentifierNode {
 private:
+	string identifier;
+	double value;
 
+public:
+	IdentifierNode(string identifier, double value) {
+		this->identifier = identifier;
+		this->value = value;
+	}
+
+	friend ostream& operator<< (ostream &, const IdentifierNode &);
 };
 
 
 class Parser {
+private:
+	char runner;
+	IdentifierLookupTable lookup;
 
+	double factor();
+	double term();
+	double expression();
+	void readLValue(string &);
+	double findValue(const string &);
+	void insertIdentifierNode(const IdentifierNode);
+	void insertIdentifierNode(const string &, const double &);
+
+	void issueError(string err) {
+		cout << "======================================================" << endl;
+		cerr << err << endl;
+		cout << "======================================================" << endl;
+
+		exit(1);
+	}
+
+public:
+	void parseStatement();	//	Entry point of the parser.
+	friend ostream& operator<< (ostream &, const Parser &);
 };
+
+void Parser::insertIdentifierNode(const string &identifier, const double& value) {
+
+}
+
+void Parser::insertIdentifierNode(const IdentifierNode idNode) {
+
+}
+
+double Parser::findValue(const string &identifier) {
+
+}
+
+void Parser::readLValue(string &lValue) {
+
+}
+
+double Parser::factor() {
+
+}
+
+double Parser::term() {
+	double fVal = factor();
+	while (1) {
+		switch (runner) {
+		case '*':
+			fVal *= factor();
+			break;
+
+		case '/':
+			fVal /= factor();
+			break;
+
+		default:
+			return fVal;
+		}
+	}
+}
+
+double Parser::expression() {
+	double termVal = term();
+	while (1) {
+		switch (runner) {
+		case '+':
+			termVal += term();
+			break;
+			
+		case '-':
+			termVal -= term();
+			break;
+
+		default:
+			return termVal;
+		}
+	}
+}
+
+void Parser::parseStatement() {
+	string lValue = "";
+	double expVal;
+	cout << "$: ";
+	cin >> runner;
+	readLValue(lValue);
+
+	if (lValue == "status") {
+		cout << this;
+	}
+	else if (lValue == "print") {
+		readLValue(lValue);	//	print the variable after the print command.
+		cout << lValue << ": " << findValue(lValue) << ln;
+	}
+	else if (lValue == "exit") {
+		exit(1);
+	}
+	else {
+		while (runner == spaceChar) cin >> runner;
+		
+		if (runner == '=') {
+			expVal = expression();
+			if (runner != endOfStatement) {
+				issueError("Expected ';' at the end of the statement");
+			}
+			else insertIdentifierNode(lValue, expVal);
+		}
+		else {
+			issueError("Invalid statement");
+		}
+	}
+}
 
 
 int main() {
 	//	Start the interpreter!
 	cout << "Hello world!!" << endl;
-
+	cout << "Press any key to continue:" << ln;
 	cin.get();
+
+	Parser parser;
+	while (1) {
+		parser.parseStatement();
+	}
 }
 
 
